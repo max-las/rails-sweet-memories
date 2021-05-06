1279ed959fed2e6e7f9dd389d422da684659eb61
Maxime <lasserre.maxime@yahoo.fr>
2021-05-06 12:15:22 +0200

    fix boostrap

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
     get "/signin" => "devise/sessions#new", as: "new_user_login" # custom path to login/sign_in
     get "/signup" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
   end
-  devise_for :users
+
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   resources :memories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
